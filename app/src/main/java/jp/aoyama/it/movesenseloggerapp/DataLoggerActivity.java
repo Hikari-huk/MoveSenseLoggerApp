e9cbe2f19f9b3391eea59c81ac8031d9d37c041c
Hikari-huk <ghikariao974@gmail.com>
2023-05-02 23:06:24 +0900

    [add] dataloggerの処理を追加

--- app/src/main/java/jp/aoyama/it/movesenseloggerapp/DataLoggerActivity.java
+++ app/src/main/java/jp/aoyama/it/movesenseloggerapp/DataLoggerActivity.java
@@ -4,16 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Message;
 import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import java.text.MessageFormat;
 
 public class DataLoggerActivity extends AppCompatActivity {
 
     public static final String SERIAL = "serial";
     public static final String MAC_ADDRESS = "macAddress";
     private final static String TAG = "DataLoggerActivity";
-
     private String serial ="";
     private String macAddress="";
+
+    //Logger Path URI
+    private static final String URI_DATA_LOGGER_CONFIG = "suunto://{0}/Mem/DataLogger/Config";
+
+    private static final String URI_DATA_ACC_CONFIG = "/Meas/Acc/13";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,4 +36,23 @@ public class DataLoggerActivity extends AppCompatActivity {
         Log.d(TAG, serial);
         Log.d(TAG, macAddress);
     }
+
+    public void onLogClickButton(View view){
+        if(view.getId()==R.id.btnLogStart){
+            displayToast("Start");
+        }else if(view.getId()==R.id.btnLogStop){
+            displayToast("Stop");
+        }
+    }
+
+    private void createDataLoggerConfig(){
+        String configUri = MessageFormat.format(URI_DATA_LOGGER_CONFIG, serial);
+
+
+    }
+
+    private void displayToast(String message){
+        Toast.makeText(getApplicationContext(),message,Toast.LENGTH_LONG);
+    }
+
 }
